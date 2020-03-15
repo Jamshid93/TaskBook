24214f099cd5ca7a620fff93ae4c4bb7e218bc0d
Jamshid93 <hamzayevjf@gmail.com>
2020-03-16 00:22:35 +0500

    find radius and areas

---
+++ begin13.py
@@ -0,0 +1,12 @@
+R1=float(input("R1= "))
+R2=float(input("R2= "))
+S1=3.14*(R1**2)
+S2=3.14*(R2**2)
+S3=S1-S2
+if (R1>R2):
+    print(S1)
+    print(S2)
+    print(S3)
+else:
+    print("It is not true!!!")
+
