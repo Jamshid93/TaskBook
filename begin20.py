fd00e28847b1afb3dc45f361a43773db2b8e23dc
Jamshid93 <hamzayevjf@gmail.com>
2020-03-17 00:38:43 +0500

    find the distance

---
+++ begin20.py
@@ -0,0 +1,7 @@
+from math import sqrt
+x1=float(input("x1= "))
+y1=float(input("y1= "))
+x2=float(input("x2= "))
+y2=float(input("y2= "))
+Answ=sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)))
+print(Answ)
