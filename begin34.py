32bb6c8c91e4f96e7b64ea28f2be52a2bc85f501
Jamshid93 <hamzayevjf@gmail.com>
2020-03-18 00:56:37 +0500

    find determine

---
+++ begin34.py
@@ -0,0 +1,10 @@
+X=float(input("X= "))
+Y=float(input("Y= "))
+A=float(input("A= "))
+B=float(input("B= "))
+onekgchoko=A/X
+onekgsuga=B/Y
+determine=onekgchoko/onekgsuga
+print(onekgchoko)
+print(onekgsuga)
+print(determine)
